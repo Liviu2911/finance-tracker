bd02c9334c1ce6858f53e43c6cdea7c2874e953d
Liviu1129 <pirvanliviu29@gmail.com>
2024-07-24 22:18:56 +0300

    server ready

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ const port = 3000;
 
 const app = express();
 
-app.get("/transactions", (req, res) => {
-  modules
-    .getTransactions()
-    .then((data) => res.status(200).send(data))
-    .catch((err) => res.status(500).send(err));
-});
+function getIt(func, url) {
+  app.get(`${url}`, (req, res) => {
+    const id = req.params.id || "-1";
+    func(id)
+      .then((data) => res.status(200).send(data))
+      .catch((err) => res.status(500).send(err));
+  });
+}
+
+getIt(modules.getCards, "/cards");
+getIt(modules.getCards, "/cards/:id");
+getIt(modules.getTransactions, "/transactions");
+getIt(modules.getTransactions, "/transactions/:id");
 
 app.listen(port, () => console.log(`App listens on port ${port}`));
--- server/modules.js
+++ server/modules.js
@@ -8,10 +8,11 @@ const client = new Pool({
   user: "liviu",
 });
 
-function getTransactions() {
+function getTransactions(id) {
+  const query = id > 0 ? ` WHERE id = ${id}` : "";
   try {
     return new Promise((res, rej) => {
-      client.query("SELECT * FROM transactions", (err, data) => {
+      client.query("SELECT * FROM transactions" + query, (err, data) => {
         if (err) rej(err);
         if (data && data.rows) res(data.rows);
         rej("Data couldn't be fetched");
@@ -22,4 +23,19 @@ function getTransactions() {
   }
 }
 
-module.exports = { getTransactions };
+const getCards = (id) => {
+  const query = id > 0 ? ` WHERE id = ${id}` : "";
+  try {
+    return new Promise((res, rej) => {
+      client.query("SELECT * FROM cards" + query, (err, data) => {
+        if (err) rej(err);
+        if (data && data.rows) res(data.rows);
+        rej("Data not found");
+      });
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+module.exports = { getTransactions, getCards };
